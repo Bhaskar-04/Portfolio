d9c04980bf29061f5d3f81a95902672dbded6c28
Bhaskar-04 <mall.bhaskar04@gmail.com>
2024-05-13 02:00:16 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {pc: `This is my PC. I work mostly in Node.js/Express these days.
+I've dedicated my upcoming time to be proficient in backend as it intrigues me the most. I would also like to learn Next.js and Python. Anyway regardless of the language, I just like programming.
+Here is my <a href="https://github.com/Bhaskar-04" target="_blank">Github</a>!`,
+"cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+"sofa-table": `That's my sofa. I like to relax here and watch YouTube and listen lowkey any kindoff music. 
+. Go hear to <a href="https://open.spotify.com/playlist/69gdoMhkUS06w8dqUUNohS?si=819a2f2d3bdd4fe4" target="_blank">my most binged playlist!</a>! `,
+tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+<a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
+<a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
+Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
+projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
+Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
+I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+exit: `If you want to exit Bhaskar's portfolio, just close the tab.`,
+};
