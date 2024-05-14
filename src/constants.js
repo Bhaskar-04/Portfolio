865d28c8db6b23a12ab45f16d304e8011c2d7912
Bhaskar-04 <mall.bhaskar04@gmail.com>
2024-05-14 21:24:11 +0530

    resume update

--- src/constants.js
+++ src/constants.js
@@ -10,7 +10,7 @@ tv: `That's my TV. I've been watching tech youtubers a lot recently like :
 <a href="https://www.youtube.com/@thesheryianscodingschool" target="_blank">Sheryians Coding School</a>, <a href="https://www.youtube.com/@mehulmpt" target="_blank">Mehul Mohan</a>,
 <a href="https://www.youtube.com/@freecodecamp" target="_blank">FreeCodeCamp.org</a> and <a href="https://www.youtube.com/@6PackProgrammer" target="_blank">6PackProgrammer</a>! (sometimes)`,
 bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-resume: `This is my desk and on it is my resume. <a href="https://github.com/Bhaskar-04/Resume/blob/main/Bhaskar's%20Resume%20(1).pdf" target="_blank">Check it out?</a>
+resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/1oY22H8UwAZAZGQRWdTHr70Bi1TBTvnD1/view?usp=sharing" target="_blank">Check it out?</a>
 Contact me at ibhaskar.email@gmail.com if you have any interesting job opportunities!`,
 projects: `I hope to get hand-on practice over actual technical products and projects and am looking forward to meet people who have great excellence in this field and hope to learn and grow as much as i can...`,
 library: `I bought this book shelf to develop my interest in reading books and so far only added and read one called something like Eat that Frog but in future I would like to read about my developing interests like typescript and Nextjs.`,
